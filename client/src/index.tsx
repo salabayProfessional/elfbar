5b5dbc8e934bf31051e22c763b34f0d5af99ef21
salabayProfessional <85737310+salabayProfessional@users.noreply.github.com>
2022-05-28 12:43:13 +0300

    establisition client part

---
+++ client/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from "./Components/App/index";
+import './index.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
+
