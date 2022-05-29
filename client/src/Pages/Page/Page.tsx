52bad7d0b5160531dffa3480c3a735376550fd6f
salabayProfessional <85737310+salabayProfessional@users.noreply.github.com>
2022-05-29 17:42:20 +0300

    appearance is added

--- client/src/Pages/Page/Page.tsx
+++ client/src/Pages/Page/Page.tsx
@@ -1,15 +1,16 @@
 import { Container } from "@mui/material";
 import React from "react";
 import "./Page.scss";
+
 interface PageProps {
-  children: React.ReactNode
+  children: React.ReactNode,
 };
 
 const Page: React.FC<PageProps> = ({
-  children
+  children,
 }) => {
   return (
-    <Container className="page">
+    <Container id="page" >
       {
         children
       }
--- client/src/Pages/SignUp/SignUp.tsx
+++ client/src/Pages/SignUp/SignUp.tsx
@@ -1,12 +1,99 @@
 import React from "react";
+import { Button, TextField } from "@mui/material";
 import Page from "../Page";
 import "./SignUp.scss";
+import { NavLink } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const SignUp: React.FC = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <Page>
-     
-    </Page>
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+        <h1 className="background-inscription">SIGN UP</h1>
+        
+        <div className="sign-up__content">
+          
+          <TextField className="primary-text-field" 
+            {...register("firstName")} 
+            required  
+            fullWidth={false} 
+            id="standard-basic-1" 
+            color="info" 
+            label="First Name" 
+            variant="standard" 
+          />
+          
+          <TextField className="primary-text-field" 
+            {...register("lastName")} 
+            required  
+            fullWidth={false} 
+            id="standard-basic-2" 
+            color="info" 
+            label="Last Name" variant="standard" 
+          />
+            
+          <TextField className="primary-text-field" 
+            {...register("surname")} 
+            required  
+            fullWidth={false} 
+            id="standard-basic-3" 
+            color="info" 
+            label="Surname" variant="standard" 
+          />
+          
+          <TextField className="primary-text-field" 
+            {...register("login")} 
+            required  
+            fullWidth={false} 
+            id="standard-basic-4" 
+            color="info" 
+            label="Login" variant="standard"   
+          />
+          
+          <TextField className="primary-text-field" 
+            {...register("email")} 
+            required  
+            fullWidth={false} 
+            id="standard-basic-4" 
+            color="info" 
+            label="Email" variant="standard" 
+          />
+            
+          <TextField className="primary-text-field" 
+            {...register("password")} 
+            required  
+            fullWidth={false} 
+            id="standard-basic-4" 
+            color="info" 
+            label="Password" variant="standard" 
+          />
+          
+          <TextField className="primary-text-field" 
+            {...register('age', { pattern: /\d+/ })} 
+            required  
+            fullWidth={false} 
+            id="standard-basic-5" 
+            color="info" 
+            label="Age" variant="standard" 
+          />
+
+        </div>
+        <div className="sign-up__bottom">
+          <Button variant="outlined" type="submit">SIGN UP</Button>
+          <NavLink to="/sign-in" className="switcher-to-sign-in">Sign In</NavLink>
+        </div>
+      </form>
+    </ Page>
   )
 };
 
